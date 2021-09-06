94eddd0ab70a4970eb76e256529157336d904d3d
axiom777 <axiom777@yandex.ru>
2021-09-06 11:54:23 +0300

    Add screenshots to game API

--- pages/api/game/[slug].js
+++ pages/api/game/[slug].js
@@ -2,12 +2,20 @@ const gameHandler = async (req, res) => {
   const { slug } = req.query;
   const key = process.env.API_KEY;
   const URL = `https://api.rawg.io/api/games/${slug}?key=${key}`;
+  const URL_SCREENSHOTS = `https://api.rawg.io/api/games/${slug}/screenshots?key=${key}`;
 
   try {
-    const response = await fetch(URL);
-    if (!response.ok) throw new Error("Response not ok");
-    const json = await response.json();
-    res.status(200).json(json);
+    const [respGame, respScreenshot] = await Promise.all([
+      fetch(URL),
+      fetch(URL_SCREENSHOTS),
+    ]);
+    if (!respGame.ok) throw new Error("Response not ok");
+    const [jsonGame, jsonScreenshot] = await Promise.all([
+      respGame.json(),
+      respScreenshot.json(),
+    ]);
+
+    res.status(200).json({ ...jsonGame, screenshots: jsonScreenshot });
   } catch (e) {
     console.error(e);
     res.status(404).json({ message: "Not Found" });
